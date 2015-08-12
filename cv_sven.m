572bfb1d059776f9eab7a475822988c63f8159ad
Kellan Fluette <kellanfluette@theseus.wireless.cougars.int>
2015-08-12 10:48:54 -0400

    try to use gpu

--- cv_sven.m
+++ cv_sven.m
@@ -7,4 +7,8 @@ function yfit = cv_sven( xtrain,ytrain,xtest, t,lambda2, options )
 % fits a SVEN model and predicts the labels for xtest
 
 beta = SVEN(xtrain',ytrain',t,lambda2); % perform SVEN on the projections to learn regression coefficients
+if options.UseParallel
+    xtest = gpuArray(xtest);
+    beta = gpuArray(beta);
+end
 yfit = xtest*beta; % perform regression
